3cc8784b221275fdd250dea6457654dfd3d86e60
Bobby Tripathi <80473945+Bobby278tr@users.noreply.github.com>
2024-01-30 00:22:03 +0530

    Update Changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 
 import './App.css';
 import AssignEditor from './components/AssignEditor';
-// import CustomEditor from './components/CustomEditor';
 
 function App() {
   return (
     <div className="App">
       <AssignEditor/>
-      {/* <CustomEditor/> */}
     </div>
   );
 }
